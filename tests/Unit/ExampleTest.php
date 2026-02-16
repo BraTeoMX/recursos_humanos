09407fe2507e44083b836cec407be2c04d4b494e
Brayam Programador <brateomx@gmail.com>
2026-02-16 14:29:03 -0600

    correcion o no se

--- tests/Unit/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-test('returns a successful response', function () {
-    // Esta línea simula un usuario logueado antes de entrar
-    $response = $this->actingAs(\App\Models\User::factory()->create())
-                     ->get('/'); 
-
-    $response->assertOk();
-});
